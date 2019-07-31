85c17d497c9731fded4565a78687ecb9cf88e0c3
akbfitra <akbarfitralistyono@gmail.com>
2019-07-31 19:43:08 +0700

    add exe 7

---
+++ exercises-7.js
@@ -0,0 +1,30 @@
+// ----------------------BINTANG JAVASCRIPT---------------------
+
+//-----------task 1 (baris bintang)--------------
+console.log('---------------task 1 (baris bintang)----------------')
+var rows1 = 5;
+
+for (var i = 0; i < rows1; i++) {
+    console.log('*')
+}
+
+//-----------task 2 (nested loop)--------------
+console.log('---------------task 2 (nested loop)----------------')
+var rows2 = 5;
+for (var i = 0; i < rows2; i++) {
+    var bintang = ''
+    for (var j = 0; j < rows2; j++) {
+        bintang += '*'
+    }
+    console.log(bintang)
+}
+//-----------task 3 (piramid bintang)--------------
+console.log('---------------task 2 (piramid bintang)----------------')
+var rows3 = 5;
+for (var i = 0; i < rows3; i++) {
+    var bintang = ''
+    for (var j = 0; j <= i; j++) {
+        bintang += '*'
+    }
+    console.log(bintang)
+}
